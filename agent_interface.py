174450f61a65ee7678f49285072e90b2faf628b4
glenrsmithjr <86325823+glenrsmithjr@users.noreply.github.com>
2024-03-08 15:36:49 -0500

    Update agent_interface.py
    Removed load function

--- agent_interface.py
+++ agent_interface.py
@@ -7,20 +7,11 @@ class DiceAdventureAgent:
     """
     Provides a uniform interface to connect agents to Dice Adventure environment.
     Developers must implement the load() and take_action() functions.
-    - init()        - optional: initialize any needed variables
-    - load()        - required: Loads and sets your agent in the 'self.agent' variable
-    - take_action() - required: Determines which action to take given a state (dict) and list of actions. Note that your
-                                agent does not need to use the list of actions, it is just provided for convenience.
+    - init():         Initialize any needed variables.
+    - take_action():  Determines which action to take given a state (dict) and list of actions. Note that your
+                      agent does not need to use the list of actions, it is just provided for convenience.
     """
     def __init__(self):
-        self.agent = None
-
-    def load(self, filepath):
-        """
-        Loads and sets the 'self.agent' variable.
-        :param filepath: (string) The file location of the agent to load
-        :return: N/A
-        """
         pass
 
     def take_action(self, state, actions):
@@ -31,4 +22,4 @@ class DiceAdventureAgent:
         :param actions: (list) A list of string action names
         :return:        (string) An action from the 'actions' list
         """
-        return choice(actions)
+        raise NotImplementedError()
