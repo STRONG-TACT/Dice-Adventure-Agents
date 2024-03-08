f6cbda79db0c8bf04050b89aa161acb366f25183
Glen Smith <glensmith@gatech.edu>
2024-03-08 11:46:23 -0500

    added documentation for functions in dice adventure env file

--- game/env/dice_adventure_python_env.py
+++ game/env/dice_adventure_python_env.py
@@ -5,12 +5,26 @@ from json import loads
 
 
 class DiceAdventurePythonEnv(Env):
+    """
+    Implements a custom gyn environment for the Dice Adventure game.
+    """
     def __init__(self,
                  player,
                  id_=0,
                  train_mode=False,
                  server="local",
                  **kwargs):
+        """
+        Init function for Dice Adventure gym environment.
+        :param player:      (string) The player that will be used to play the game.
+        :param id_:         (int) An optional ID parameter to distinguish this environment from others.
+        :param train_mode:  (bool) A helper parameter to switch between training mode and play mode. When we test agents,
+                                   we will use a "play" mode, where the step function simply takes an action and returns
+                                   the next state.
+        :param server:      (string) Determines which game version to use. Can be one of {local, unity}.
+        :param kwargs:      (dict) Additional keyword arguments to pass into Dice Adventure game. Only applies when
+                                   'server' is 'local'.
+        """
         self.config = self.config = loads(open("game/config/main_config.json", "r").read())
         self.player = player
         self.id = id_
@@ -32,6 +46,13 @@ class DiceAdventurePythonEnv(Env):
             self.game = DiceAdventure(**self.kwargs)
 
     def step(self, action):
+        """
+        Applies the given action to the game. Determines the next observation and reward,
+        whether the training should terminate, whether training should be truncated, and
+        additional info.
+        :param action:  (string) The action produced by the agent
+        :return:        (dict, float, bool, bool, dict) See description
+        """
         next_state = self.execute_action(self.player, action)
 
         reward = self._get_reward()
@@ -48,19 +69,39 @@ class DiceAdventurePythonEnv(Env):
         return new_obs, reward, terminated, truncated, info
 
     def close(self):
+        """
+        close() function from standard gym environment. Not implemented.
+        :return: N/A
+        """
         pass
 
     def render(self, mode='console'):
+        """
+        Prints the current board state of the game. Only applies when `self.server` is 'local'.
+        :param mode: (string) Determines the mode to use (not used)
+        :return: N/A
+        """
         if self.server == "local":
             self.game.render()
 
     def reset(self, **kwargs):
+        """
+        Resets the game. Only applies when `self.server` is 'local'.
+        :param kwargs:  (dict) Additional arguments to pass into local game server
+        :return:        (dict, dict) The initial state when the game is reset, An empty 'info' dict
+        """
         if self.server == "local":
             self.game = DiceAdventure(**self.kwargs)
         obs = self.get_state()
         return obs, {}
 
     def execute_action(self, player, game_action):
+        """
+        Executes the given action for the given player.
+        :param player:      (string) The player that should take the action
+        :param game_action: (string) The action to take
+        :return:            (dict) The resulting state after taking the given action
+        """
         if self.server == "local":
             self.game.execute_action(player, game_action)
         else:
@@ -69,6 +110,10 @@ class DiceAdventurePythonEnv(Env):
         return self.get_state()
 
     def get_state(self):
+        """
+        Gets the current state of the game.
+        :return: (dict) The game state
+        """
         if self.server == "local":
             state = self.game.get_state()
         else:
@@ -78,5 +123,9 @@ class DiceAdventurePythonEnv(Env):
 
     @staticmethod
     def _get_reward():
+        """
+        Calculates the reward the agent should receive based on action taken.
+        :return: (int) The reward
+        """
         return 0
 
