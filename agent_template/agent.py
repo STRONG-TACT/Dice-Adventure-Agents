7047686f8509d1ea17e732add6ac18c21a51364f
glenrsmithjr <86325823+glenrsmithjr@users.noreply.github.com>
2025-02-28 14:21:04 -0500

    Update agent.py
    updated character code parameter to character id

--- agent_template/agent.py
+++ agent_template/agent.py
@@ -8,21 +8,20 @@ class DiceAdventureAgent:
     - take_action():  Determines which action to take given a state (dict) and list of actions. Note that your
                       agent does not need to use the list of actions, it is just provided for convenience.
     """
-    def __init__(self, character_name, character_code):
+    def __init__(self, character_name, character_id):
         """
-        Initialize any needed variables.
+        Initialize any needed variables. The character_name and character_id arguments specify the name and ID
+        of the character this agent will play as.
         :param character_name: (string) The character the agent will play as
-        :param character_code: (string) The character code corresponding to the character
+        :param character_id: (string) The character ID corresponding to the character
 
-        Player code index:
-        {
-            "Dwarf" : "C1",
-            "Giant" : "C2",
-            "Human" : "C3"
-        }
+        Player ID mappings:
+            Dwarf : C11
+            Giant : C21
+            Human : C31
         """
         self.character = character_name
-        self.character_code = character_code
+        self.character_id = character_id
 
     def take_action(self, state, actions):
         """
