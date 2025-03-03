c14aff9db5d749bb76b9708e0d125ef1d5c646ea
glenrsmithjr <86325823+glenrsmithjr@users.noreply.github.com>
2025-03-02 22:59:32 -0500

    Update play_game.py

--- play_game.py
+++ play_game.py
@@ -1,24 +1,21 @@
 from time import sleep
 from examples.random_agent.dice_adventure_gym_env import DiceAdventureGymEnv
+from examples.random_agent.dice_adventure_gym_env import get_player_id
 from examples.random_agent.agent import DiceAdventureAgent
 
 
-SERVER = "local"
-
-
 def main():
-    # Set up environment
-    env = DiceAdventurePythonEnv(player="dwarf", port='4649')
-    # Load agents
-    players = env.get_player_names()
-    agents = [DiceAdventureAgent(p, env.get_player_code(player=p, port='4650')) for p in players]
+    players = ['dwarf', 'giant', 'human']
+    # Set up environments
+    envs = [DiceAdventurePythonEnv(player=p, port='4649') for p in players]
+    # Create agents
+    agents = [DiceAdventureAgent(p, get_player_id(player=p, port='4649')) for i, p in enumerate(players)]
 
     while True:
-        for agent in agents:
-            state = env.get_state()
-            action = agent.take_action(state=state, actions=env.get_actions())
-            env.execute_action(game_action=action)
-        env.render()
+        for i in range(len(agents)):
+            state = envs[i].get_state()
+            action = agents[i].take_action(state=state, actions=env.get_actions())
+            envs[i].execute_action(game_action=action)
         sleep(.1)
 
 
