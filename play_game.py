c76ee62ac26da31dcba550b70ce95174c74a7cc9
glenrsmithjr <86325823+glenrsmithjr@users.noreply.github.com>
2025-03-02 22:44:14 -0500

    Update play_game.py

--- play_game.py
+++ play_game.py
@@ -8,16 +8,16 @@ SERVER = "local"
 
 def main():
     # Set up environment
-    env = DiceAdventurePythonEnv(server=SERVER, state_version="fow")
+    env = DiceAdventurePythonEnv(player="dwarf", port='4649')
     # Load agents
     players = env.get_player_names()
-    agents = [DiceAdventureAgent(p, env.get_player_code(p)) for p in players]
+    agents = [DiceAdventureAgent(p, env.get_player_code(player=p, port='4650')) for p in players]
 
     while True:
         for agent in agents:
-            state = env.get_state(player=agent.character)
+            state = env.get_state()
             action = agent.take_action(state=state, actions=env.get_actions())
-            env.execute_action(player=agent.character, game_action=action)
+            env.execute_action(game_action=action)
         env.render()
         sleep(.1)
 
