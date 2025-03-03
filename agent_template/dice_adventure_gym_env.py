f5e4318eb54a115cd7a22456c302b194b31883b8
glenrsmithjr <86325823+glenrsmithjr@users.noreply.github.com>
2025-03-02 21:56:45 -0500

    Update dice_adventure_gym_env.py

--- agent_template/dice_adventure_gym_env.py
+++ agent_template/dice_adventure_gym_env.py
@@ -1,7 +1,7 @@
 from json import loads
 from typing import Any, Tuple, Union, Dict
 from gymnasium import Env
-import game.unity_socket as unity_socket
+from gane.unity_socket import UnityWebSocket
 
 
 class DiceAdventurePythonEnv(Env):
@@ -22,6 +22,8 @@ class DiceAdventurePythonEnv(Env):
         self.port = port
         self.train_mode = train_mode
         self.socket_url = "ws://localhost:{}/hmt/{}".format(self.port, self.player)
+        self.unity_socket = UnityWebSocket(self.socket_url)
+
         self.actions = ["up", "down", "left", "right", "wait", "undo", "submit", "pinga", "pingb", "pingc", "pingd"]
         self.player_names = ["dwarf", "giant", "human"]
 
@@ -59,12 +61,12 @@ class DiceAdventurePythonEnv(Env):
 
         Note: Although this framework is usually used for RL models, users can develop any kind of model with this code.
         """
-        next_state = self.execute_action(self.player, action)
+        next_state = self.execute_action(action)
 
         reward = self._get_reward()
 
         # new_obs, reward, terminated, truncated, info
-        terminated = next_state["status"] == "Done"
+        terminated = next_state[0]["status"] == "Done"
         if terminated:
             new_obs, info = self.reset()
         else:
@@ -79,7 +81,7 @@ class DiceAdventurePythonEnv(Env):
         :param action:  The action produced by the agent
         :return:        The resulting state
         """
-        return self.execute_action(self.player, action)
+        return self.execute_action(action)
 
     def close(self):
         """
@@ -100,15 +102,14 @@ class DiceAdventurePythonEnv(Env):
         """
         pass
 
-    def execute_action(self, player: str, game_action: str):
+    def execute_action(self, game_action: str):
         """
         Executes the given action for the given player.
-        :param player:      The player being controlled
         :param game_action: The action to take
         :return:            The resulting state after taking the given action
         """
         # TODO CAPTURE RESPONSE AND RETURN TO USER
-        unity_socket.execute_action(self.socket_url, game_action)
+        self.unity_socket.execute_action(game_action)
         return self.get_state()
 
     def get_state(self):
@@ -127,7 +128,7 @@ class DiceAdventurePythonEnv(Env):
             view (i.e. not obscured by black or gray squares), but static objects such as walls, stones, and traps,
             and shrines are returned if they've been previously observed.
         """
-        return unity_socket.get_state(self.socket_url)
+        return _simplify_state(self.unity_socket.get_state(), self.player)
 
     def get_actions(self):
         return self.actions
@@ -136,14 +137,88 @@ class DiceAdventurePythonEnv(Env):
         return self.player_names
 
     @staticmethod
-    def get_player_code(player):
-        codes = {"Dwarf": "C11", "Giant": "C21", "Human": "C31"}
-        return codes[player]
-
-    @staticmethod
     def _get_reward():
         """
         Calculates the reward the agent should receive based on action taken.
         :return: (int) The reward
         """
         return 0
+
+
+#############
+# UTILITIES #
+#############
+def _simplify_state(state: dict, player: str) -> list[dict]:
+    """
+    Simplifies the game state into a list of dictionary objects, each representing an object in the game.
+    :param state: The game state
+    :param player: The player the state is related to
+    :return: The modified state as a list of dictionaries
+    """
+    state['content']['gameData']['id'] = 'gameData'
+    game_data_obj = state['content']['gameData']
+    scene = [game_data_obj] + state['content']['scene']
+
+    return _add_sight_status(scene, player, game_data_obj)
+
+
+def _add_sight_status(scene: list[dict], player: str, game_data_obj: dict) -> list[dict]:
+    """
+    Modifies the state by adding whether objects are visible or hidden. It also adds 'Cell' objects with an 'unexplored'
+    status to represent grid squares that have not yet been observed.
+    :param scene: The list of objects in the state
+    :param player: The name of the player
+    :param game_data_obj: The state object containing high level information about the current state of the game
+    :return: The modified scene list
+    """
+    player_obj = _find_player_obj(scene, player)
+    x_lower = player_obj['x'] - player_obj['sightRange']
+    x_upper = player_obj['x'] + player_obj['sightRange']
+    y_lower = player_obj['y'] - player_obj['sightRange']
+    y_upper = player_obj['y'] + player_obj['sightRange']
+
+    all_cells = {(i, j) for i in range(game_data_obj['boardWidth']) for j in range(game_data_obj['boardHeight'])}
+    scene_cells = {(obj.get('x'), obj.get('y')) for obj in scene}
+    unexplored = all_cells - scene_cells
+
+    for obj in scene:
+        x, y = obj.get('x'), obj.get('y')
+        if x is None or y is None:
+            continue
+        if x_lower <= x <= x_upper and y_lower <= y <= y_upper:
+            obj['sight_status'] = 'visible'
+        else:
+            obj['sight_status'] = 'hidden'
+
+    for i, pos in enumerate(unexplored):
+        scene.append({'id': f'UE{i}', 'entityType': 'Cell',
+                      'objKey': 'UE', 'sight_status': 'unexplored',
+                      'x': pos[0], 'y': pos[1]})
+    return scene
+
+
+def _find_player_obj(scene: list[dict], player: str) -> [dict, None]:
+    """
+    Locates the player's object dictionary in the scene list.
+    :param scene: The list of objects in the state
+    :param player: The name of the player to be returned
+    :return: The player dictionary object
+    """
+    pid = _get_player_id(player)
+    for obj in scene:
+        if obj.get('id') == pid:
+            return copy(obj)
+
+
+def _get_player_id(player: str) -> str:
+    """
+    Gets the player ID.
+    :param player: The player whose ID will be returned
+    :return: The player's ID
+    """
+    ids = {"dwarf": "C11", "giant": "C21", "human": "C31"}
+    return ids[player]
+
+
+
+
