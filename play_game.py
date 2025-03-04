755fecd238fbcbfac99568ff3e54c67adb8b9ec1
glenrsmithjr <86325823+glenrsmithjr@users.noreply.github.com>
2025-03-04 01:44:27 -0500

    Update play_game.py
    modified to use env to play game

--- play_game.py
+++ play_game.py
@@ -1,4 +1,3 @@
-from time import sleep
 from examples.random_agent.dice_adventure_gym_env import DiceAdventureGymEnv
 from examples.random_agent.dice_adventure_gym_env import get_player_id
 from examples.random_agent.agent import DiceAdventureAgent
@@ -6,18 +5,13 @@ from examples.random_agent.agent import DiceAdventureAgent
 
 def main():
     # Set up environment
-    env = DiceAdventurePythonEnv(port='4649', train_mode=False)
+    env = DiceAdventureGymEnv(port='4649',
+                                 game_executable_filepath='DiceAdventure.exe')
     # Load agents
-    players = env.get_player_names()
-    agents = [DiceAdventureAgent(p, get_player_id(player=p)) for p in players]
-
-    while True:
-        for i in range(len(agents)):
-            state = env.get_state(players[i])
-            action = agent.take_action(state=state, actions=env.get_actions())
-            env.execute_action(game_action=action)
-        env.render()
-        sleep(.1)
+    agents = [('dwarf', DiceAdventureAgent('dwarf', get_player_id('dwarf'))),
+              ('giant', DiceAdventureAgent('giant', get_player_id('giant'))),
+              ('human', DiceAdventureAgent('human', get_player_id('human')))]
+    env.play(agents)
 
 
 if __name__ == "__main__":
